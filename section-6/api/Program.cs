bfeb554e809f6f6a0f7994d17e03ba57210817c4
Luan Douglas Maia da Silva <douglasmaia.mn.sl@gmail.com>
2022-10-04 09:30:26 -0300

    section 6

---
+++ section-6/api/Program.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapPost("/products", (Product product) =>
+{
+    ProductRepository.Add(product);
+    return Results.Created($"/products/{product.Code}", product.Code);
+});
+
+
+app.MapGet("/products/{code}", ([FromRoute] string code) =>
+{
+    var product = ProductRepository.GetBy(code);
+    if (product != null)
+        return Results.Ok(product);
+    return Results.NotFound();
+});
+
+app.MapPut("/products", (Product product) =>
+{
+    var productSaved = ProductRepository.GetBy(product.Code);
+    productSaved.Name = product.Name;
+
+    Results.Ok();
+});
+
+app.MapDelete("/products/{code}", ([FromRoute] string code) =>
+{
+    var product = ProductRepository.GetBy(code);
+    ProductRepository.Remove(product);
+
+    Results.Ok();
+});
+
+
+app.Run();
+
+public static class ProductRepository
+{
+    public static List<Product> Products { get; set; }
+
+    public static void Add(Product product)
+    {
+        if (Products == null)
+            Products = new List<Product>();
+        Products.Add(product);
+    }
+
+    public static Product GetBy(string code)
+    {
+        return Products.FirstOrDefault(p => p.Code == code);
+    }
+
+    public static void Remove(Product product)
+    {
+        Products.Remove(product);
+    }
+}
+
+
+public class Product
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
